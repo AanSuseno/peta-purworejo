0c57bb0af0bd502a0aef0f179f9f0f9def4c4298
Aan <aansuseno822@gmail.com>
2026-09-03 17:53:47 +0700

    fix pg pool

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,16 +1,21 @@
 import "dotenv/config";
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from "@prisma/adapter-pg";
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+import pg from 'pg';
 
+const { Pool } = pg;
+
+// 1. Buat connection pool dari library 'pg'
 const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ 
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false 
+});
 
-const adapter = new PrismaPg({
-    connectionString
-});
-    
-const prisma = new PrismaClient({
-    adapter
-});
+// 2. Masukkan pool ke dalam adapter Prisma
+const adapter = new PrismaPg(pool);
+
+// 3. Inisialisasi PrismaClient dengan adapter
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
